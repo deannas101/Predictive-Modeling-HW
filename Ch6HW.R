e5cc15151e55c37e4b7442d3871d95e999ae0264
deannas101 <52871125+deannas101@users.noreply.github.com>
2020-10-20 18:45:04 -0400

    Update Ch6HW.R

---
+++ Ch6HW.R
@@ -0,0 +1,23 @@
+library(AppliedPredictiveModeling)
+library(caret)
+libray(e1071)
+library(tidyverse)
+
+####6.1####
+
+data(tecator)
+?tecator
+
+#absorp - absorbance data
+#endpoints - percentages of water, fat, protein (cols 1-3)
+
+colnames(absorp) <- c(1:100)
+pca_absorp <- predict(preProcess(absorp, method = c("center", "scale", "pca")), absorp) #default cutoff is 95%
+
+#dimensions after pca: 215 x 2
+
+trainingRows <- createDataPartition(absorp[,1], p=0.80, list=FALSE)
+testingRows <- createDataPartition(absorp[,1], p=0.20, list=FALSE)
+
+training <- absorp[trainingRows,]
+testing <- absorp[testingRows,]
