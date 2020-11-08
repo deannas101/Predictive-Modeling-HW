ef010246bff47becd550aa2d4adc626a0d9be1ca
deannas101 <52871125+deannas101@users.noreply.github.com>
2020-11-08 18:24:24 -0500

    Create Ch12HW.R

---
+++ Ch12HW.R
@@ -0,0 +1,38 @@
+library(AppliedPredictiveModeling)
+library(caret)
+library(glmnet)
+library(MASS)
+library(modeldata)
+library(pamr)
+library(pls)
+library(pROC)
+library(rms)
+library(sparseLDA)
+library(subselect)
+library(tidyverse)
+
+####12.1####
+
+data(hepatic)
+
+#a) I would create a training and testing set by splitting the data using
+#stratified random sampling and using injury as the factor
+
+#b) I would optimize using....
+
+####12.1c####
+
+####12.1d####
+
+#imp plot
+
+####12.3####
+
+data(mlc_churn)
+
+####12.3a####
+
+#outcome: churn, yes/no
+
+#pairwise plot
+#correlation plot
