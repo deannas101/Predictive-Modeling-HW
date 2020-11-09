456c1e72abaaee57b55340b283dbec8a32e32633
deannas101 <52871125+deannas101@users.noreply.github.com>
2020-11-09 17:06:39 -0500

    Update Ch12HW.R

--- Ch12HW.R
+++ Ch12HW.R
@@ -1,11 +1,13 @@
 library(AppliedPredictiveModeling)
 library(caret)
+library(corrplot)
 library(glmnet)
 library(MASS)
 library(modeldata)
 library(pamr)
 library(pls)
 library(pROC)
+library(RColorBrewer)
 library(rms)
 library(sparseLDA)
 library(subselect)
@@ -18,13 +20,32 @@ data(hepatic)
 #a) I would create a training and testing set by splitting the data using
 #stratified random sampling and using injury as the factor
 
-#b) I would optimize using....
+#b) I would optimize using.... specificity or sensitivity etc
 
 ####12.1c####
 
-####12.1d####
+fullSet <- bio
+fullSet[,185:377] <- chem
+fullSet[, 378] <- injury
+#injury is named V378
 
-#imp plot
+redSet <- predict(preProcess(fullSet, method=c("nzv", "corr")), fullSet)
+
+trainingRows <- createDataPartition(fullSet[,378], p=0.80, list=FALSE)
+
+fullTraining <- fullSet[trainingRows,]
+fulTtesting <- fullSet[-trainingRows,]
+
+trainingRows <- createDataPartition(redSet[,238], p=0.80, list=FALSE)
+
+redTraining <- redSet[trainingRows,]
+redTesting <- redSet[-trainingRows,]
+
+####12.1d####
+#edit me!
+nnImp <- varImp(nnFit, scale = FALSE)
+nnImp
+plot(nnImp, top = 20, main = "Neural Network")
 
 ####12.3####
 
@@ -36,3 +57,12 @@ data(mlc_churn)
 
 #pairwise plot
 #correlation plot
+
+pairs(cleanChurn)
+
+####12.3c####
+
+trainingRows <- createDataPartition(mlc_churn$churn, p=0.80, list=FALSE)
+
+fullTraining <- mlc_churn[trainingRows,]
+fulTtesting <- mlc_churn[-trainingRows,]
