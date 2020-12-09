956571c961de8a8c2da690c70ea9bc0c7ece6e85
deannas101 <52871125+deannas101@users.noreply.github.com>
2020-12-08 19:55:57 -0500

    Finish Hw 13

--- Ch13HW.R
+++ Ch13HW.R
@@ -31,20 +31,7 @@ ctrl <- trainControl(
   savePredictions = TRUE
 )
 
-# Quadratic Discriminant Analysis NOT FUNCTIONAL
-set.seed(80)
-qda_model <- train(
-  x = training_set[, 1:184],
-  y = training_set$V185,
-  method = "qda",
-  metric = "Kappa",
-  trControl = ctrl
-)
-
-qda_model
-plot(qda_model, main = "Quadratic Discriminant Analysis")
-qda_pred <- predict(qda_model, newdata = testing_set[, 1:184])
-confusionMatrix(data = qda_pred, reference = testing_set$V185)
+# Quadratic Discriminant Analysis - n>p, so this model is not applicable
 
 # Regularized Discriminant Analysis
 set.seed(80)
@@ -53,7 +40,7 @@ rda_model <- train(
   y = training_set$V185,
   method = "rda",
   metric = "Kappa",
-  tuneGrid = expand.grid(.lambda = 1:3, .gamma = 1:3),
+  tuneGrid = expand.grid(.lambda = 1:5, .gamma = 1:5),
   trControl = ctrl
 )
 
@@ -69,7 +56,8 @@ mda_model <- train(
   y = training_set$V185,
   method = "mda",
   metric = "Kappa",
-  tuneGrid = expand.grid(.subclasses = 1:3),
+  preProc = c("knnImpute", "nzv"),
+  tuneGrid = expand.grid(.subclasses = 1:5),
   trControl = ctrl
 )
 
@@ -81,20 +69,20 @@ confusionMatrix(data = mda_pred, reference = testing_set$V185)
 # Neural Networks
 nnet_grid <- expand.grid(.size = 1:20, .decay = c(0, .1, 1, 2))
 max_size <- max(nnet_grid$.size)
-num_weights <- (max_size * (184 + 1) + (max_size+1)*3) #184 predictors, 3 classes
+num_weights <- (max_size * (184 + 1) + (max_size + 1) * 3) # 184 predictors, 3 classes
 
 set.seed(80)
 nnet_model <- train(
-    x = training_set[, 1:184],
-    y = training_set$V185,
-    method = "nnet",
-    metric = "Kappa",
-    preProc = c("center", "scale", "spatialSign", "nzv"),
-    tuneGrid = nnet_grid,
-    trace = FALSE,
-    maxit = 2000,
-    MaxNWts = num_weights,
-    trControl = ctrl
+  x = training_set[, 1:184],
+  y = training_set$V185,
+  method = "nnet",
+  metric = "Kappa",
+  preProc = c("center", "scale", "spatialSign", "nzv"),
+  tuneGrid = nnet_grid,
+  trace = FALSE,
+  maxit = 2000,
+  MaxNWts = num_weights,
+  trControl = ctrl
 )
 
 nnet_model
@@ -102,38 +90,24 @@ plot(nnet_model, main = "Neural Networks")
 nnet_pred <- predict(nnet_model, newdata = testing_set[, 1:184])
 confusionMatrix(data = nnet_pred, reference = testing_set$V185)
 
-# Flexible Discriminant Analysis
-mars_grid <- expand.grid(.degree = 1:2, .nprune = 2:38)
-
-set.seed(80)
-fda_model <- train(
-    x = training_set[, 1:184],
-    y = training_set$V185,
-    method = "fda",
-    metric = "Kappa",
-    tuneGrid = mars_grid,
-    trControl = ctrl
-)
-
-fda_model
-plot(fda_model, main = "Flexible Discriminant Analysis")
-fda_pred <- predict(fda_model, newdata = testing_set[, 1:184])
-confusionMatrix(data = fda_pred, reference = testing_set$V185)
+# Flexible Discriminant Analysis - n>p, so this model is not applicable
 
 # Support Vector Machine (Radial)
 sigma_reduced_range <- sigest(as.matrix(training_set[, 1:184]))
-svm_reduced_grid <- expand.grid(.sigma = sigma_reduced_range[1],
-                               .C = 2^(seq(-4, 6)))
+svm_reduced_grid <- expand.grid(
+  .sigma = sigma_reduced_range[1],
+  .C = 2^(seq(-4, 6))
+)
 
 set.seed(80)
 svm_model <- train(
-    x = training_set[, 1:184],
-    y = training_set$V185,
-    method = "svmRadial",
-    metric = "Kappa",
-    tuneGrid = svm_reduced_grid,
-    fir = FALSE,
-    trControl = ctrl
+  x = training_set[, 1:184],
+  y = training_set$V185,
+  method = "svmRadial",
+  metric = "Kappa",
+  tuneGrid = svm_reduced_grid,
+  fir = FALSE,
+  trControl = ctrl
 )
 
 svm_model
@@ -144,13 +118,13 @@ confusionMatrix(data = svm_pred, reference = testing_set$V185)
 # K-Nearest Neighbors
 set.seed(80)
 knn_model <- train(
-    x = training_set[, 1:184],
-    y = training_set$V185,
-    method = "knn",
-    metric = "Kappa",
-    preProc = c("center", "scale"),
-    tuneGrid = data.frame(.k = 1:50),
-    trControl = ctrl
+  x = training_set[, 1:184],
+  y = training_set$V185,
+  method = "knn",
+  metric = "Kappa",
+  preProc = c("center", "scale", "nzv"),
+  tuneGrid = data.frame(.k = 1:50),
+  trControl = ctrl
 )
 
 knn_model
@@ -161,16 +135,20 @@ confusionMatrix(data = knn_pred, reference = testing_set$V185)
 # Naive Bayes
 set.seed(80)
 nb_model <- train(
-    x = training_set[, 1:184],
-    y = training_set$V185,
-    method = "nb",
-    metric = "Kappa",
-    preProc = c("center", "scale"),
-    tuneGrid = data.frame(.fL = 2,.usekernel = TRUE,.adjust = TRUE),
-    trControl = ctrl
+  x = training_set[, 1:184],
+  y = training_set$V185,
+  method = "nb",
+  metric = "Kappa",
+  preProc = c("center", "scale", "nzv"),
+  tuneGrid = data.frame(.fL = c(0, .1, 1, 2), .usekernel = TRUE, .adjust = TRUE),
+  trControl = ctrl
 )
 
 nb_model
 plot(nb_model, main = "Naive Bayes")
 nb_pred <- predict(nb_model, newdata = testing_set[, 1:184])
 confusionMatrix(data = nb_pred, reference = testing_set$V185)
+
+# variable importance
+imp_vars <- varImp(knn_model, scale = FALSE)
+plot(imp_vars, top = 5, main = "K-Nearest Neighbors")
